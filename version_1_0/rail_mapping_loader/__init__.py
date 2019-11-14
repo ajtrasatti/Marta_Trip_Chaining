fd2e242ba13869dc22d5b9442e986b43dcd29380
Josh <jemorgan1000@gmail.com>
2019-11-14 14:59:40 -0500

    RailMapper

---
+++ version_1_0/rail_mapping_loader/__init__.py
@@ -0,0 +1,43 @@
+
+import pandas as pd
+
+class RailMappingLoader:
+    """
+
+    """
+    def load_rail_mappings(self, path):
+        """
+
+        :return:
+        """
+        rail_mappings = pd.read_csv(path)
+        return rail_mappings
+
+
+    def clean_rail_mappings(self, rail_mappings):
+        """
+
+        :param rail_mappings:
+        :return:
+        """
+        rail_mappings.update(rail_mappings.stop_name.apply(lambda x: x[:len(x) - 7].strip()))
+        rail_mappings.drop_duplicates(subset=[
+                                              'stop_name'],inplace=True)
+        return rail_mappings
+
+    def fit_2_network(self, rail_mappings, network):
+        """
+
+        :param rail_mappings:
+        :param network:
+        :return:
+        """
+        x = []
+        for _ in rail_mappings.itertuples():
+            route = network.routes["RAIL"]
+            stop = route.tree.query_point(_.stop_lat,_.stop_lon)
+            x.append(stop.id)
+        rail_mappings.insert(len(rail_mappings.columns), "MEGA_STOP", x)
+        print(rail_mappings.head())
+        return rail_mappings
+
